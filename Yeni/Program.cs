daef9f5f9ab4d6ebd642b31d428e88da4d5773c3
Enes Özer <eenesozerr@icloud.com>
2021-01-19 21:48:58 +0300

    Add project files.

---
+++ Yeni/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SepetMenager sepetMenager = new SepetMenager();
+            Product product1 = new Product();
+
+            product1.Name = "Elma";
+            product1.Explanation = "Amasya Elma";
+            product1.Price = 10;
+            product1.Id = 1;
+
+            Product product2 = new Product();
+            product2.Name = "visne";
+            product2.Explanation = "Afyon Visne";
+            product2.Price = 15;
+            product2.Id = 2;
+
+            Product[] products = new Product[] { product1, product2 };
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.Name);
+                Console.WriteLine(product.Explanation);
+                Console.WriteLine(product.Price);
+                sepetMenager.Add(product);
+                Console.WriteLine(" ");
+            }
+            Console.WriteLine("-------Metotlar-------");
+        }
+    }
+}
---
+++ Yeni/SepetMenager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetMenager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.Name + " Sepete eklendi ");
+        }
+    }
+}
